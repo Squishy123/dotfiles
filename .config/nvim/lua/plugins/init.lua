b1805cea49cf2f659ff7b83388628c4f3abb5d62
Christian Wang <christian.gnaw@gmail.com>
2024-04-19 10:57:57 -0400

    add

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,69 +1,69 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        -- event = 'BufWritePre', -- uncomment for format on save
-        config = function()
-            require "configs.conform"
-        end,
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "typescript-language-server",
+        "graphql-language-service-cli",
+        "intelephense",
+        "prettiered",
+      },
     },
-    -- These are some examples, uncomment them if you want to see them work!
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-            require "configs.lspconfig"
-        end,
-    },
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "lua-language-server",
-                "stylua",
-                "html-lsp",
-                "css-lsp" ,
-                "prettier",
-                "typescript-language-server",
-                "graphql-language-service-cli",
-                "intelephense",
-                "prettiered",                
-            },
-        },
-    },
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "vim",
-                "lua",
-                "vimdoc",
-                "html",
-                "css",
-                "graphql",
-                "javascript",
-                "typescript",
-                "tsx",
-                "php"
-            },
-        },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "graphql",
+        "javascript",
+        "typescript",
+        "tsx",
+        "php",
+      },
     },
-    {
-        "christoomey/vim-tmux-navigator",
-        lazy = false,
-        -- cmd = {
-        --     "TmuxNavigateLeft",
-        --     "TmuxNavigateDown",
-        --     "TmuxNavigateUp",
-        --     "TmuxNavigateRight",
-        --     "TmuxNavigatePrevious",
-        -- },
-        -- keys = {
-        --     { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        --     { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-        --     { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-        --     { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-        --     { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-        -- },
-    }
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    -- cmd = {
+    --     "TmuxNavigateLeft",
+    --     "TmuxNavigateDown",
+    --     "TmuxNavigateUp",
+    --     "TmuxNavigateRight",
+    --     "TmuxNavigatePrevious",
+    -- },
+    -- keys = {
+    --     { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    --     { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    --     { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    --     { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    --     { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    -- },
+  },
 }
