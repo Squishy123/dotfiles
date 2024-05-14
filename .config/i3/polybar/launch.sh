13147f28484c06feb0fcd1fd501594a848a13492
Chris Wang <christian.gnaw@gmail.com>
2024-05-14 10:26:36 -0400

    Added polybar

---
+++ .config/i3/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload -c ~/.config/i3/polybar/config.ini navbar &
+done
