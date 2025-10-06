0eef14fc7ca5f09d22a00b66a67d3f7fd2049e98
Chris Wang <christian.gnaw@gmail.com>
2025-10-06 09:48:54 -0400

    backup

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,4 +1,4 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
@@ -17,12 +17,12 @@ end
 
 -- intelephense
 lspconfig.intelephense.setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-    init_options = {
-          globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense' 
-    }
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  init_options = {
+    globalStoragePath = os.getenv "HOME" .. "/.local/share/intelephense",
+  },
 }
 
 -- typescript
